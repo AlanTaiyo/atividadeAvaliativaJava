92c972ed5b83c1f7e4647e1964e70459d55a4028
PizzaModaDaCasa <87745411+AlanPizza@users.noreply.github.com>
2023-08-07 11:53:33 -0300

    Main.java

--- banco/Main.java
+++ banco/Main.java
@@ -1,4 +1,4 @@
-package banco;
+package treinoConta;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -11,60 +11,83 @@ public class Main {
 		
 		Conta Conta = null;
 		
-		System.out.println("Bem vindo a sua conta bancária, como primeiro acesso me digite seu número: ");
-		int numeroConta = leitura.nextInt();
-		System.out.println("Qual o seu nome? ");
-		leitura.nextLine();
+		System.out.println("Este programa irá ler sua conta bancária.");
+		System.out.println("Digite o seu nome: ");
 		String nomeConta = leitura.nextLine();
 		
-		System.out.println("Você quer realizar seu primeiro depósito?");
-		System.out.println("1 -> para SIM");
-		System.out.println("0 -> para NÃO");
-		System.out.println();
+		System.out.println("Digite o número da conta: ");
+		int numeroConta = leitura.nextInt();
 		
-		boolean continuarLoop = true; //sustentar meu loop kkkkkkkkkk
+		System.out.println("Você deseja realizar seu primeiro depósito?");
+		System.out.println("1 - SIM");
+		System.out.println("2 - NÃO");
 		
-		while (continuarLoop == true) {
+		boolean loop = true;
+		
+		while (loop == true) {
+			int escolha = leitura.nextInt();
 			
-			int primeiroDeposito = leitura.nextInt();
-			
-			if (primeiroDeposito == 0) {
-				Conta = new Conta(numeroConta, nomeConta);
-				System.out.println(Conta);
-				continuarLoop = false;
-			}
-			else if (primeiroDeposito == 1) {
-				System.out.print("Digite a quantidade que você quer depositar em reais: $");
+			if (escolha == 1) {
+				System.out.println("Digite a quantidade de dinheiro que você quer depositar:");
 				double depositoInicial = leitura.nextDouble();
-				System.out.println("Deposito realizado com sucesso!");
-				Conta = new Conta(numeroConta, nomeConta, depositoInicial);
-				System.out.println(Conta);
-				continuarLoop = false;
+				Conta = new Conta(nomeConta, numeroConta, depositoInicial);
+				loop = false;
 			}
+			
+			else if (escolha == 2) {
+				Conta = new Conta(nomeConta, numeroConta);
+				loop = false;
+			}
+			
 			else {
-				System.out.println();
-				System.out.println("Por favor, digite 0 para SIM ou 1 para NÃO. Tente novamente.");
-				continuarLoop = true;
+				System.out.println("Por favor, digite apenas número 1(SIM) ou 2(NÃO).");
+				continue;
 			}
 		}
 		
-		System.out.println();
-		System.out.println("Digite um valor para se fazer um deposito na sua conta:");
-		double valorDepositado = leitura.nextDouble();
-		Conta.deposito(valorDepositado);
-		System.out.println();
-		System.out.println("- INFORMAÇÕES ATUALIZADAS -");
-		System.out.println(Conta);
+		boolean loop2 = true;
 		
-		System.out.println();
-		System.out.println("Digite um valor para se fazer um saque na sua conta:");
-		double valorSacado = leitura.nextDouble();
-		Conta.saque(valorSacado);
-		System.out.println();
-		System.out.println("- INFORMAÇÕES ATUALIZADAS -");
-		System.out.println(Conta);
 		
+		while (loop2 == true) {
+			
+			System.out.println(Conta);
+			System.out.println();
+			System.out.println(Conta.exibeMensagem());;
+			System.out.println();
+			
+			int escolha2 = leitura.nextInt();
+			
+			if (escolha2 == 1) {
+				System.out.println("Digite o valor que você quer depositar: ");
+				double novoDeposito = leitura.nextDouble();
+				Conta.deposito(novoDeposito);
+		
+				continue;
+				
+			}
+			
+			else if (escolha2 == 2) {
+				System.out.println("Digite o valor que você quer sacar: ");
+				double novoSaque = leitura.nextDouble();
+				Conta.saque(novoSaque);
+				
+				continue;
+			}
+			
+			else if (escolha2 == 3) {
+				System.out.println("Saindo do programa...");
+				loop2 = false;
+			}
+			
+			else {
+				System.out.println();
+				System.out.println("Por favor tecle apenas uma das opções a seguir:");
+				continue;
+			}
+		}
+
 		leitura.close();
 	}
 
 }
+
